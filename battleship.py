99339a6cdfaeb18a3a517cbf0a270c60a32cb645
Javier Marco <javi.marco@ucv.es>
2014-09-03 14:10:25 +0200

    Modificacion IF seleccion fuera de rango

--- battleship.py
+++ battleship.py
@@ -33,7 +33,7 @@ for turn in range(4):
         print "Congratulations! You sunk my battleship!"
         break
     else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+        if guess_row not in range(5) or guess_col not in range(5):
             print "Oops, that's not even in the ocean."
         elif board[guess_row][guess_col] == "X":
             print "You guessed that one already."
